698ea8258453f53609961739d27da8b73167160f
Marielle Rectin <157998090+onlymarkive@users.noreply.github.com>
2024-12-14 13:50:39 +0800

    Update ElectronicItem.java

--- src/ElectronicItem.java
+++ src/ElectronicItem.java
@@ -1,11 +1,23 @@
+import java.util.Random;
+
 public class ElectronicItem extends Item {
+    private static final String[] ECO_TIPS = {
+        "Choose energy-efficient electronics to reduce power consumption.",
+        "Recycle your old electronics to keep them out of landfills.",
+        "Buy products with minimal packaging to reduce waste."
+    };
+
     public ElectronicItem(String name, double price, int quantity) {
         super(name, price, quantity);
     }
 
     @Override
-    public void displayDetails() {
-        System.out.println("Electronic Item: " + getName() + ", Price: PHP " + getPrice() +
-                ", Quantity: " + getQuantity());
+    public String getEcoFriendlyTip() {
+        return getRandomTip(ECO_TIPS);
+    }
+
+    private String getRandomTip(String[] tips) {
+        Random rand = new Random();
+        return tips[rand.nextInt(tips.length)];
     }
 }
