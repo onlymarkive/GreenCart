35982fa13ef366313b70b7256da3b979808a84c8
Marielle Rectin <157998090+onlymarkive@users.noreply.github.com>
2024-12-14 13:51:05 +0800

    Update GroceryItem.java

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,11 +1,23 @@
+import java.util.Random;
+
 public class GroceryItem extends Item {
+    private static final String[] ECO_TIPS = {
+        "Opt for locally grown produce to reduce carbon footprint.",
+        "Buy in bulk to minimize packaging waste.",
+        "Choose organic products to support sustainable farming practices."
+    };
+
     public GroceryItem(String name, double price, int quantity) {
         super(name, price, quantity);
     }
 
     @Override
-    public void displayDetails() {
-        System.out.println("Grocery Item: " + getName() + ", Price: PHP " + getPrice() +
-                ", Quantity: " + getQuantity());
+    public String getEcoFriendlyTip() {
+        return getRandomTip(ECO_TIPS);
+    }
+
+    private String getRandomTip(String[] tips) {
+        Random rand = new Random();
+        return tips[rand.nextInt(tips.length)];
     }
 }
